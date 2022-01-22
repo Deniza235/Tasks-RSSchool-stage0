d7a0fb95bca56e789af8081cae1812c45bb58d9e
github-deniza235 <lobashova.mariya@mail.ru>
2022-01-22 12:20:47 +0300

    fix: change style for open burger icon

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#burger').addEventListener('click', function() {
     document.querySelector('#burger').classList.toggle('is-active');
+    document.querySelector('.header__bg-overlay').classList.toggle('shadow');
   })
 })
